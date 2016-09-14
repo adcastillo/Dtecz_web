1dd34853300c148f4c496c3db98d428770757280
adcastillo <Angel Castillo>
2016-09-14 17:10:28 -0500

    added web pages controllers

---
+++ app/controllers/web_pages_controller.rb
@@ -0,0 +1,13 @@
+class WebPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def service
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'web_pages/home'
+
+  get 'web_pages/about'
+
+  get 'web_pages/contact'
+
+  get 'web_pages/service'
+
 	root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
